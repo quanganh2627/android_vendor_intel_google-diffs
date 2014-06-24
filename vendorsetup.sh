338aa3fb97112b61fad74f46053d6afa9e16bfe8
Yong Yao <yong.yao@intel.com>
2014-06-24 22:17:53 +0800

    l_pdk
    Change-Id: I0b969b5ad145f59e30bc080cf66187c0165347a2
    Signed-off-by: Yong Yao <yong.yao@intel.com>
    Reviewed-on: https://android.intel.com/211195

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,10 +5,10 @@
 if [ -z "$KK_FUSION_PLATFORM_ZIP" ]; then
     
     # Set the platform.zip file.
-    export KK_FUSION_PLATFORM_ZIP=$(pwd)/vendor/pdk/mini_x86/mini_x86-userdebug/platform/platform.zip
+    export PDK_FUSION_PLATFORM_ZIP=$(pwd)/vendor/pdk/mini_x86/mini_x86-userdebug/platform/platform.zip
     
     # Patch the tree
-    bash $(pwd)/vendor/intel/google_diffs/scripts/apply_patch.sh android_4.4
+    bash $(pwd)/vendor/intel/google_diffs/scripts/apply_patch.sh l_pdk
     
     # RE-Execute the contents of any vendorsetup.sh files we can find.
     for f in `test -d device && find device -maxdepth 6 -name 'vendorsetup.sh' 2> /dev/null` \
